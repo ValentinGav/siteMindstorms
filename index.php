2d37e4460a72fbb041b7ce435548e0d7bfe722a1
ValentinGav <51400412+ValentinGav@users.noreply.github.com>
2019-06-05 10:59:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <? php
-		include_once ("page_principale.html");
-		include_once ("page_code.html");
-		include_once ("page_photos.html");
+		include_once ("pageprincipale.html");
+		include_once ("pagecode.html");
+		include_once ("pagephotos.html");
 		include_once ("téléchargement.PNG");
 		include_once ("menu.css");
 ?>
