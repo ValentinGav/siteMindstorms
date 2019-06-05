69de3c004608437f337521a8a1860a641a49cd0f
ValentinGav <51400412+ValentinGav@users.noreply.github.com>
2019-06-05 10:52:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<? php
+		include_once ("page_principale.html");
+		include_once ("page_code.html");
+		include_once ("page_photos.html");
+		include_once ("téléchargement.PNG");
+		include_once ("menu.css");
+?>
